c0c704d0e83ba8ee84712a5779dc866f2b765ba4
Rahul Prajapat <rahul.prajapat7921@gmail.com>
2025-01-09 13:15:06 +0530

    handle delimiters

--- lib/calculate_string.rb
+++ lib/calculate_string.rb
@@ -2,6 +2,13 @@ class CalculateString
   def add(numbers)
     return 0 if numbers.empty?
     
+    if numbers.start_with?("//")
+      delimiter_end = numbers.index("\n")
+      delimiter = numbers[2..delimiter_end-1]
+      numbers = numbers[delimiter_end+1..-1]
+      return numbers.split(delimiter).map(&:to_i).sum
+    end
+
     numbers.split(/[,\n]/).map(&:to_i).sum
   end
 end
--- spec/lib/calculate_string_spec.rb
+++ spec/lib/calculate_string_spec.rb
@@ -21,5 +21,10 @@ describe CalculateString do
       calculator = described_class.new
       expect(calculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'handles custom delimiters' do
+      calculator = described_class.new
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
