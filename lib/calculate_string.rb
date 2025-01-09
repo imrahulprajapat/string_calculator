fcbd5dd08926512df06206c8245f2cb2619238f4
Rahul Prajapat <rahul.prajapat7921@gmail.com>
2025-01-09 13:42:24 +0530

    final code and breaking previous code into more readable formot

--- lib/calculate_string.rb
+++ lib/calculate_string.rb
@@ -2,20 +2,40 @@ class CalculateString
   def add(numbers)
     return 0 if numbers.empty?
 
-    if numbers.include?("//")
-      delimiter_end = numbers.index("\n")
-      delimiter = numbers[delimiter_end-1..delimiter_end-1]
-      numbers = numbers[delimiter_end+1..-1]
-      return numbers.split(delimiter).map(&:to_i).sum
+    if custom_delimiter?(numbers)
+      delimiter, numbers = extract_delimiter_and_numbers(numbers)
+      return sum_of_numbers(numbers, delimiter)
     end
 
-    numbers_list = numbers.split(/[,\n]/).map(&:to_i)
-    negatives = numbers_list.select { |num| num < 0 }
-
-    if negatives.any?
-      raise "negative numbers not allowed #{negatives.join(',')}"
-    end
+    numbers_list = parse_numbers(numbers)
+    validate_negatives(numbers_list)
 
     numbers_list.sum
+  end
+
+  private
+
+  def custom_delimiter?(numbers)
+    numbers.include?("//")
+  end
+
+  def extract_delimiter_and_numbers(numbers)
+    delimiter_end = numbers.index("\n")
+    delimiter = numbers[delimiter_end-1..delimiter_end-1]
+    numbers = numbers[delimiter_end + 1..-1]
+    [delimiter, numbers]
+  end
+
+  def sum_of_numbers(numbers, delimiter)
+    numbers.split(delimiter).map(&:to_i).sum
+  end
+
+  def parse_numbers(numbers)
+    numbers.split(/[,\n]/).map(&:to_i)
+  end
+
+  def validate_negatives(numbers_list)
+    negatives = numbers_list.select { |num| num < 0 }
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
   end
 end
